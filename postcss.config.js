4d15ebb066399313e12ecff8b0213e7697b6dfd2
Joel Hooks <joelhooks@gmail.com>
2020-05-19 17:38:14 -0700

    tailwindui changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let environment = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
@@ -12,3 +12,19 @@ module.exports = {
     }),
   ],
 }
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  )
+}
+module.exports = environment
